1149f36866e3bde24bb7e54232c2abbd6d0f8501
Tramnal <tramnal@hotmail.com>
2023-09-19 21:20:22 +0800

    add constants

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ import requests
 import time
 
 
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = '6277525733:AAFJFbr4CpmCDNvYrm2sMamNjnAOI_gkDqg'
+TEXT = 'Салам Алейкум!'
+MAX_COUNTER = 100
 
+offset = -2
+counter = 0
+chat_id: int

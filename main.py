31527b96afa9d1c855e86a5a60ee34e7aab9a92c
Tramnal <tramnal@hotmail.com>
2023-09-19 22:00:11 +0800

    upgraded to cat-bot

--- main.py
+++ main.py
@@ -3,18 +3,17 @@ import time
 
 
 API_URL = 'https://api.telegram.org/bot'
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
 BOT_TOKEN = '6277525733:AAFJFbr4CpmCDNvYrm2sMamNjnAOI_gkDqg'
-TEXT = 'Салам Алейкум!'
-MAX_COUNTER = 10
+ERROR_TEXT = 'Здесь должна была быть картинка с котиком :('
 
 offset = -2
 counter = 0
-chat_id: int
+cat_response: requests.Response
+cat_link: str
 
-while counter < MAX_COUNTER:
-
-    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
-
+while counter < 100:
+    print('attempt =', counter)
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
     print(updates)
@@ -23,7 +22,13 @@ while counter < MAX_COUNTER:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+            cat_response = requests.get(API_CATS_URL)
+
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
 
     time.sleep(1)
     counter += 1

5eb19cd4c0dc7ed620576cd2f20ea7284bafa146
Tramnal <tramnal@hotmail.com>
2023-09-19 21:38:37 +0800

    add print updates and reduced constant

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 API_URL = 'https://api.telegram.org/bot'
 BOT_TOKEN = '6277525733:AAFJFbr4CpmCDNvYrm2sMamNjnAOI_gkDqg'
 TEXT = 'Салам Алейкум!'
-MAX_COUNTER = 100
+MAX_COUNTER = 10
 
 offset = -2
 counter = 0
@@ -16,6 +16,8 @@ while counter < MAX_COUNTER:
     print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
 
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    print(updates)
 
     if updates['result']:
         for result in updates['result']:

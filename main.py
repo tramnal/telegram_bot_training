729be82f54021ff1156949c49c0f285515190e8f
Tramnal <tramnal@hotmail.com>
2023-09-20 22:21:19 +0800

    upgraded to polling server

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import requests
 import time
 
-
 API_URL = 'https://api.telegram.org/bot'
-API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
 BOT_TOKEN = '6277525733:AAFJFbr4CpmCDNvYrm2sMamNjnAOI_gkDqg'
-ERROR_TEXT = 'Здесь должна была быть картинка с котиком :('
 
 offset = -2
-counter = 0
-cat_response: requests.Response
-cat_link: str
+updates: dict
 
-while counter < 100:
-    print('attempt =', counter)
+
+def do_something() -> None:
+    print('Был апдейт')
+
+
+while True:
+    start_time = time.time()
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
     print(updates)
@@ -21,14 +21,8 @@ while counter < 100:
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            cat_response = requests.get(API_CATS_URL)
+            do_something()
 
-            if cat_response.status_code == 200:
-                cat_link = cat_response.json()[0]['url']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
-            else:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
-
-    time.sleep(1)
-    counter += 1
+    time.sleep(3)
+    end_time = time.time()
+    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')

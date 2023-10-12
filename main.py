c3b947bb3bffe105510dcf4a8d8ea461f8ced729
Tramnal <tramnal@hotmail.com>
2023-10-12 23:29:13 +0800

    added send_sticker_echo handler, all handler registering via decorators

--- main.py
+++ main.py
@@ -11,11 +11,13 @@ dp = Dispatcher()
 
 
 # This handler will trigger command "/start"
+@dp.message(Command(commands=['start']))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
 # This handler will trigger command '/help'
+@dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь, и в ответ '
@@ -24,20 +26,22 @@ async def process_help_command(message: Message):
 
 
 # This handler will trigger when sending a photo to the bot
+@dp.message(F.photo)
 async def send_photo_echo(message: Message):
     await message.reply_photo(message.photo[0].file_id)
 
 
+# This handler will trigger when sending stickers to the bot
+@dp.message(F.sticker)
+async def send_sticker_echo(message: Message):
+    await message.reply_sticker(message.sticker.file_id)
+
+
 # This handler will trigger any text messages, except for the "/start" and "/help" commands
+@dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
 
-
-# Registering handlers
-dp.message.register(process_start_command, Command(commands='start'))
-dp.message.register(process_help_command, Command(commands='help'))
-dp.message.register(send_photo_echo, F.photo)
-dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)

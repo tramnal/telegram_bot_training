41d5e640c907fa15b9f17930857e8105877c7e56
Tramnal <tramnal@hotmail.com>
2023-10-13 00:10:23 +0800

    updated with send_copy method to handle all message types

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 from dotenv import dotenv_values
@@ -25,58 +25,16 @@ async def process_help_command(message: Message):
     )
 
 
-# This handler will trigger when sending a photo to the bot
-@dp.message(F.photo)
-async def send_photo_echo(message: Message):
-    await message.reply_photo(message.photo[0].file_id)
-
-
-# This handler will trigger when sending stickers to the bot
-@dp.message(F.sticker)
-async def send_sticker_echo(message: Message):
-    await message.reply_sticker(message.sticker.file_id)
-
-
-# This handler will trigger when sending an animation to the bot
-@dp.message(F.animation)
-async def send_animation_echo(message: Message):
-    await message.reply_animation(message.animation.file_id)
-
-
-# This handler will trigger when sending voice messages to the bot
-@dp.message(F.voice)
-async def send_voice_echo(message: Message):
-    await message.reply_voice(message.voice.file_id)
-
-
-# This handler will trigger when sending videos to the bot
-@dp.message(F.video)
-async def send_video_echo(message: Message):
-    await message.reply_video(message.video.file_id)
-
-
-# This handler will trigger when sending audios to the bot
-@dp.message(F.audio)
-async def send_audio_echo(message: Message):
-    await message.reply_audio(message.audio.file_id)
-
-
-# This handler will trigger when sending documents to the bot
-@dp.message(F.document)
-async def send_document_echo(message: Message):
-    await message.reply_document(message.document.file_id)
-
-
-# This handler will trigger when sending location to the bot
-@dp.message(F.location)
-async def send_location_echo(message: Message):
-    await message.reply_location(message.location.latitude, message.location.longitude)
-
-
-# This handler will trigger any text messages, except for the "/start" and "/help" commands
+# This handler will trigger any messages, except for the "/start" and "/help" commands
 @dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(
+            text='Данный тип апдейтов не поддерживается '
+                 'методом send_copy'
+        )
 
 
 if __name__ == '__main__':
